a8541d21893c4287a6d84a5999a37d51414d7c5f
dinaemad88 <dinaemad287@gmail.com>
2023-10-26 21:12:04 +0300

    lab_testing

--- src/app/lab/pipe/square.pipe.isolation.spec.ts
+++ src/app/lab/pipe/square.pipe.isolation.spec.ts
@@ -1,5 +1,15 @@
-describe("1-square pipe (isolation) testing:",()=>{
+import { SquarePipeForLab } from "./square.pipe";
 
-    it("expect to return 16 when passing 4",()=>{})
-    it("expect to return 'Not a number' when passing wrong parameter",()=>{})
+describe("1-square pipe (isolation) testing:",()=>{
+    let SquarePipe: SquarePipeForLab;
+    beforeEach(()=>{
+
+        SquarePipe = new SquarePipeForLab();
+    })
+    it("expect to return 16 when passing 4",()=>{
+        expect(SquarePipe.transform(4)).toBe(16);
+    })
+    it("expect to return 'Not a number' when passing wrong parameter",()=>{
+        expect(SquarePipe.transform('aaa')).toBe('Not a number');
+    })
 })
